562b9440e55ad840ab885d0bb1318b65251f7a26
Pedro Igor <peuigor09@gmail.com>
2025-01-06 10:32:51 -0300

    Resolver conflitos de merge no arquivo my_app.py

--- my_app.py
+++ my_app.py
@@ -1,54 +1,3 @@
-<<<<<<< HEAD
-@app.route('/remove-background', methods=['POST'])
-def remove_background():
-    try:
-        file = request.files['file']
-        file.save('input_image.png')
-        print("Imagem recebida na rota de remoção de fundo")
-
-        # Verificar a estrutura de diretórios
-        for root, dirs, files in os.walk("/app"):
-            print(f"Diretório: {root}")
-            for filename in files:
-                print(f"Arquivo: {filename}")
-
-        # Verificar arquivos no diretório esperado
-        print("Listar arquivos no diretório /app/U-2-Net")
-        if os.path.exists("/app/U-2-Net"):
-            for filename in os.listdir("/app/U-2-Net"):
-                print(f"Arquivo: {filename}")
-
-        # Caminhos potenciais para o script
-        potential_paths = [
-            '/app/U-2-Net/u2net_test.py'
-        ]
-
-        script_path = None
-        for path in potential_paths:
-            if os.path.isfile(path):
-                script_path = path
-                break
-
-        if script_path is None:
-            print("Erro: Caminho do script não encontrado.")
-            return jsonify({"message": "Caminho do script não encontrado"}), 500
-
-        print("Caminho do script:", script_path)
-
-        result = subprocess.run([sys.executable, script_path, 'input_image.png'], capture_output=True, text=True)
-        print("Resultado da execução do script:", result.stdout)
-        print("Erros da execução do script:", result.stderr)
-
-        processed_image_path = 'input_image_processed.png'
-        if os.path.exists(processed_image_path):
-            return send_file(processed_image_path, mimetype='image/png')
-        else:
-            return jsonify({"message": "Erro ao processar a imagem"}), 500
-
-    except Exception as e:
-        print("Erro no processamento da imagem:", str(e))
-        return jsonify({"message": "Erro no processamento da imagem"}), 500
-=======
 from flask import Flask, request, send_file, jsonify
 from flask_cors import CORS
 import subprocess
@@ -84,15 +33,15 @@ def remove_background():
                 print(f"Arquivo: {filename}")
 
         # Verificar arquivos no diretório esperado
-        print("Listar arquivos no diretório /app/app/U-2-Net")
-        if os.path.exists("/app/app/U-2-Net"):
-            for filename in os.listdir("/app/app/U-2-Net"):
+        print("Listar arquivos no diretório /app/U-2-Net")
+        if os.path.exists("/app/U-2-Net"):
+            for filename in os.listdir("/app/U-2-Net"):
                 print(f"Arquivo: {filename}")
 
         # Caminhos potenciais para o script
         potential_paths = [
-            '/app/app/U-2-Net/u2net_test.py',
-            '/app/U-2-Net/u2net_test.py'
+            '/app/U-2-Net/u2net_test.py',
+            '/app/app/U-2-Net/u2net_test.py'
         ]
 
         script_path = None
@@ -140,4 +89,3 @@ def get_all_images():
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
->>>>>>> 8a207bb449046dccaeb388f2645898a1dfe25ffb
