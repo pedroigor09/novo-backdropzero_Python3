f5e468ea4d1d53e870b2b6e25355ebfdcad425c5
Pedro Igor <peuigor09@gmail.com>
2025-01-06 09:30:27 -0300

    Atualizando o my_app.py

--- my_app.py
+++ my_app.py
@@ -1,24 +1,3 @@
-from flask import Flask, request, send_file, jsonify
-from flask_cors import CORS
-import subprocess
-import os
-import sys
-
-app = Flask(__name__)
-CORS(app, resources={r"/*": {"origins": "*"}})
-
-@app.route('/')
-def home():
-    print("Requisição recebida na rota de boas-vindas")
-    return "Bem-vindo ao seu servidor Python! O backend está no ar."
-
-@app.route('/upload', methods=['POST'])
-def upload_image():
-    file = request.files['file']
-    file.save('uploaded_image.png')
-    print("Imagem recebida e salva como uploaded_image.png")
-    return jsonify({"message": "Upload bem-sucedido"}), 200
-
 @app.route('/remove-background', methods=['POST'])
 def remove_background():
     try:
@@ -33,14 +12,13 @@ def remove_background():
                 print(f"Arquivo: {filename}")
 
         # Verificar arquivos no diretório esperado
-        print("Listar arquivos no diretório /app/app/U-2-Net")
-        if os.path.exists("/app/app/U-2-Net"):
-            for filename in os.listdir("/app/app/U-2-Net"):
+        print("Listar arquivos no diretório /app/U-2-Net")
+        if os.path.exists("/app/U-2-Net"):
+            for filename in os.listdir("/app/U-2-Net"):
                 print(f"Arquivo: {filename}")
 
         # Caminhos potenciais para o script
         potential_paths = [
-            '/app/app/U-2-Net/u2net_test.py',
             '/app/U-2-Net/u2net_test.py'
         ]
 
@@ -69,23 +47,3 @@ def remove_background():
     except Exception as e:
         print("Erro no processamento da imagem:", str(e))
         return jsonify({"message": "Erro no processamento da imagem"}), 500
-
-@app.route('/images/all', methods=['GET'])
-def get_all_images():
-    try:
-        image_directory = 'images'
-
-        if not os.path.exists(image_directory):
-            return jsonify({"message": "Diretório de imagens não encontrado"}), 500
-
-        image_list = os.listdir(image_directory)
-        image_list = [img for img in image_list if img.endswith(('.png', '.jpg', '.jpeg', '.gif'))]
-
-        return jsonify({"images": image_list}), 200
-
-    except Exception as e:
-        print("Erro ao listar imagens:", str(e))
-        return jsonify({"message": "Erro ao listar imagens"}), 500
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
